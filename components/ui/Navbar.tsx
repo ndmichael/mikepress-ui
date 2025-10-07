daa62044d3b7cecb30b942373ad74e17d595bda5
ndmichael <ukejemicheal@gmail.com>
2025-10-07 15:37:43 +0100

    new shadcn ui added

--- components/ui/Navbar.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Navbar = () => {
-    return (
-        <div className='bg-primary py-2 px-5 flex justify-between text-white dark:bg-slate-700 '>Navbar</div>
-    )
-};
-
-export default Navbar;
