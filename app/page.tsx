edd05d2c13debb184078c844753546a4babb9cff
ndmichael <ukejemicheal@gmail.com>
2025-10-08 14:37:52 +0100

    Added back button

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,43 @@
-import { Button } from "@/components/ui/button";
+import DashboardCard from "@/components/dashboard/DashboardCard";
+import PostsTable from "@/components/posts/PostsTable";
+import AnalyticsChart from "@/components/dashboard/AnalyticsChart";
+import { Folder, MessageCircle, Newspaper, User } from "lucide-react";
 
 export default function Home() {
   return (
     <>
-      <h1 className="text-4xl text-amber-600">Dashboard</h1>
-      <Button
-        variant='outline'
-        size='lg'
-      >
-        CLICK ME
-      </Button>
+      <div className="flex flex-col justify-between gap-5 md:flex-row">
+        <DashboardCard 
+          title="Posts" 
+          icon={<Newspaper className="text-slate-500" 
+          size={72} />} count={200} 
+        />
+
+        <DashboardCard 
+          title="Categories" 
+          icon={<Folder className="text-slate-500" 
+          size={72} />} 
+          count={12} 
+        />
+
+        <DashboardCard 
+          title="Users" 
+          icon={<User className="text-slate-500" 
+          size={72} />} 
+          count={200} 
+        />
+
+        <DashboardCard 
+          title="Comments" 
+          icon={<MessageCircle className="text-slate-500" 
+          size={72} />} 
+          count={1200} 
+        />
+      </div>
+
+      <AnalyticsChart />
+
+      <PostsTable limit={5} />
     </>
   );
 }
