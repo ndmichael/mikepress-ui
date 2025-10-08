a7fb5bd8e13638d5c29ab5ea17e75b8f4d177c59
ndmichael <ukejemicheal@gmail.com>
2025-10-08 14:36:39 +0100

    layout to display items properly

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/ui/Navbar";
+import Navbar from "@/components/Navbar";
+import Sidebar from "@/components/Sidebar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +28,18 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <Navbar />
-        {children}
+        <div className="flex">
+          {/** Sidebar */}
+          <div className="hidden md:block h-[100vh] w-[300px]">
+            <Sidebar/>
+          </div>
+
+          {/** Contents */}
+          <div className="p-5 w-full md:max-w-[1140px]">
+            {children}
+          </div>
+        </div>
+        
       </body>
     </html>
   );
